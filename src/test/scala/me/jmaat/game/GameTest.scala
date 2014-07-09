be5bcd5bf24de452f2b65510171490284557fef5
Justin <jxm262@gmail.com>
2014-07-09 14:10:38 -0400

    Modifying .gitignore
    Adding build.sbt
    
    Adding dummy test file

---
+++ src/test/scala/me/jmaat/game/GameTest.scala
@@ -0,0 +1,13 @@
+package me.jmaat.game
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class GameTest extends FunSuite {
+
+  test("dummy test") {
+	  assert("hello" === "hello")
+  }
+} 
