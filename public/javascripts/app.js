62c7871d975826d744291b4c7a0130c0d12a2f97
Justin Maat <jxm262@gmail.com>
2014-08-01 19:55:26 -0400

    adding function to capture direction of keypress

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,3 +2,21 @@
  * Application custom JS
  */
 
+
+(function(){
+	$(document).keydown(function(e){
+		function getDirection() {
+			switch(e.which) {
+				case 37: return "left";
+				case 38: return "up";
+				case 39: return "right";
+				case 40: return "down";
+				default: return "";
+			}
+		}
+		e.preventDefault();
+	
+		console.log(getDirection());
+		
+	});
+})();
