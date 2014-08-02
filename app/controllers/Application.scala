876a059cc504b60e446de354c51e9cd31d56af5c
Justin Maat <jxm262@gmail.com>
2014-08-01 21:13:13 -0400

    Adding javascript function to accept input and return game result. Displays output to console

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,36 +3,27 @@ package controllers
 import play.api._
 import play.api.mvc._
 import game.Game._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object Application extends Controller {
 
-  var nums = List(0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  val gameRequest: Reads[(String, List[Int])] = {
+    (JsPath \ "direction").read[String] and
+    (JsPath \ "numbers").read[List[Int]]
+  }.tupled
+  
+  def test = Action(parse.json) { request => 
+   request.body.validate(gameRequest).map{ 
+      case (direction, numbers) => 
+        Ok(Json.obj("numbers" -> next(direction, numbers) ))  
+    }.recoverTotal{
+      e => BadRequest("Detected error:"+ JsError.toFlatJson(e))
+    }
+  }
 
-  def reset() = nums = List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-      
-  def printNums(l: List[Int]) = ((for (x <- l.grouped(4).toList) yield (x mkString ",\t")) mkString "\n")
   def index = Action {
-        Ok(views.html.index("Your new application is ready."))
-	 
-//    reset()
-//    
-//    val step0 = printNums(nums)
-//    
-//    nums = next("up", nums)
-//    val step1 = printNums(nums)
-//
-//    nums = next("up", nums)
-//    val step3 = printNums(nums)
-//	
-//    nums = next("up", nums)
-//	val step5 = printNums(nums)
-//    
-//	nums = next("up", nums)
-//	val step7 = printNums(nums)
-//	
-//	val res = step0 + "\n\n\n" + step1  + "\n\n\n" + step3 + "\n\n\n" + step5 + "\n\n\n" + step7
-//	
-//    Ok(res)
+    Ok(views.html.index("Your new application is ready."))
   }
 
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,13 @@
  * Application custom JS
  */
 
+var randomPos = (Math.ceil(Math.random() * 16)) - 1 ;
+var nums = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+nums[randomPos] = 2;
 
 (function(){
+	
+	
 	$(document).keydown(function(e){
 		function getDirection() {
 			switch(e.which) {
@@ -11,12 +16,28 @@
 				case 38: return "up";
 				case 39: return "right";
 				case 40: return "down";
-				default: return "";
+				default: return;
 			}
 		}
 		e.preventDefault();
 	
-		console.log(getDirection());
-		
+		$.ajax({
+			  type: "POST",
+			  url: "/test",
+			  contentType: "application/json; charset=utf-8",
+			  data: JSON.stringify({direction: getDirection(), numbers: nums}),
+			  dataType: "json",
+			  success: function(data, textStatus, jqXHR){
+				  nums = data.numbers;
+				  
+				  for(var i=0; i < 16; i+=4){
+					 console.log(nums[i] + "\t" + nums[i+1] + "\t" + nums[i+2] + "\t" + nums[i+3]);
+				  }
+			  },
+			  error: function(jqXHR, textStatus, errorThrown){
+				  console.log("fail");
+				  console.log(errorThrown);
+			  }
+			});
 	});
 })();
