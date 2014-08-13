4e5ec74554cff2c83d5253ceb2913be7a8286c4c
Justin Maat <jxm262@gmail.com>
2014-08-13 00:30:57 -0400

    Updating front end to display score

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,10 +4,11 @@
 
 var randomPos = (Math.ceil(Math.random() * 16)) - 1 ;
 var nums = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-//nums[randomPos] = 2;
 
 (function(){
 	
+	//init
+	move("up");
 	
 	$(document).keydown(function(e){
 		switch(e.which) {
@@ -16,30 +17,41 @@ var nums = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 			case 39: return move("right");
 			case 40: return move("down");
 		}
-	
-		function move(direction){
-			$.ajax({
-			  type: "POST",
-			  url: "/games/2048-Scala/move",
-			  contentType: "application/json; charset=utf-8",
-			  data: JSON.stringify({direction: direction, numbers: nums}),
-			  dataType: "json",
-			  success: function(data, textStatus, jqXHR){
-				  nums = data.numbers;
-				  $("h1").each(function(idx){
-					 var num = (nums[idx] == 0) ? '' : nums[idx];
-					 
-					 $(this).parent().css("background-color", getColor(num));
-					 $(this).replaceWith("<h1>" + num + "</h1>");
-				  });
-			  },
-			  error: function(jqXHR, textStatus, errorThrown){
-				  console.log("fail");
-				  console.log(errorThrown);
-			  }
-			});
-		};
 	});
+
+	function move(direction){
+		$.ajax({
+		  type: "POST",
+		  url: "/games/2048-Scala/move",
+		  contentType: "application/json; charset=utf-8",
+		  data: JSON.stringify({direction: direction, numbers: nums}),
+		  dataType: "json",
+		  success: function(data, textStatus, jqXHR){
+			  window.justin = data;
+			  nums = data.numbers;
+			  updateGrid(nums);
+			  updateScore(data.score);
+		  },
+		  error: function(jqXHR, textStatus, errorThrown){
+			  console.log("fail");
+			  console.log(errorThrown);
+		  }
+		});
+	};
+	
+	function updateScore(score){
+		var newScore = parseInt($("#score").text(), 10) + parseInt(score, 10);
+		$("#score").text(newScore);
+	};
+	
+	function updateGrid(nums){
+		$("h1").each(function(idx){
+			var num = (nums[idx] == 0) ? '' : nums[idx];
+			
+			$(this).parent().css("background-color", getColor(num));
+			$(this).replaceWith("<h1>" + num + "</h1>");
+		});
+	};
 	
 	//TODO this is pretty jacked up, but its getting late.  Redo this
 	function getColor(num){
@@ -51,8 +63,6 @@ var nums = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 		var g = 250;
 		var b = 250;
 		 
-		if(num != 0) {console.log(num  + "   " + Math.log(num * 10) + "  " + colorMultiple * 35);}
-
 		if(colorMultiple == 0){
 			return shade(r-40, g-40, b-40);
 		}
